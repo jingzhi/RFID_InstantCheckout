f74a4fab94ce91a0a31e370c57590e1c064d0022
jingzhi <jingzhi_li@outlook.com>
2017-03-02 19:17:30 +0000

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,62 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QFile>
+#include<QMessageBox>
+#include<QTextStream>
+#include<QVector>
+#include<QtCore>
+#include<QDebug>
+
+nodeptr search( nodeptr hdlist);
+void printall ( nodeptr  hdlist);
+int totalprice (nodeptr  hdlist);
+void readreaderfile( QVector<QString> & readerinput);
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QFile file2( "C:/Users/jingz/Desktop/RFID_GUI/RFID_Checkout/resource/file/database.txt" );
+
+    if (!file2.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this,"ERROR","file not open");
+     }
+
+ while(!file2.atEnd()){
+     QString y =file2.readLine();
+     if(y!=" "){
+    ui->listWidget_2->addItem(y);}
+ }
+file2.close();
+}
+
+void MainWindow::on_readerfile_clicked()
+{
+    QFile file( "C:/Users/jingz/Desktop/RFID_GUI/RFID_Checkout/resource/file/GUI/rfid-reader.log" );
+
+    if (!file.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this,"ERROR","file not open");
+     }
+
+ while(!file.atEnd()){
+     QString y =file.readLine();
+     if(y[0] == '['&&y[1]!=','){
+    ui->listWidget->addItem(y);
+     }
+ }
+file.close();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,15 @@ namespace Ui {
 class MainWindow;
 }
 
+struct node {
+    QString uid;
+    QString name;
+    QString price;
+    node* next;
+};
+
+typedef node* nodeptr;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -15,6 +24,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_readerfile_clicked();
+
+    
 private:
     Ui::MainWindow *ui;
 };
